70ab6efc7283d5f09a7eccec6e580c13a6ef20d4
Jo Pham <josephpham@ou.edu>
2020-09-01 12:17:00 -0500

    Completed the program

--- Participations/Participation2_SumOf3Numbers/Program.cs
+++ Participations/Participation2_SumOf3Numbers/Program.cs
@@ -6,7 +6,27 @@ namespace Participation2_SumOf3Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter First Number");
+            double FirstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            double SecondNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Third Number");
+            double ThirdNumber = Convert.ToDouble(Console.ReadLine());
+
+            double sumAnswer = (FirstNumber + SecondNumber + ThirdNumber);
+
+            double constant = 7.777;
+
+            double mulAnswer = (sumAnswer * constant);
+
+            Console.WriteLine("The sum of these 3 numbers is " +
+                sumAnswer.ToString("N3"));
+
+            Console.WriteLine("The sum multiplied by the constant is " +
+                mulAnswer.ToString("N3"));
+
+
+
         }
     }
 }
