5bca7ed793fdbe7c322d8ff0d6588909d7d4801b
Jo Pham <josephpham@ou.edu>
2020-09-08 11:58:31 -0500

    Added Conditional Statement

---
+++ Participations/Fizz Buzz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fizz_Buzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1, 101);
+           
+            if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+
+            Console.WriteLine(number);
+
+        }
+    }
+}
