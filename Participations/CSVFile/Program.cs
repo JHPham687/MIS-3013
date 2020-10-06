d71a2f3857acc16b6130f22c8c0df40c480debdb
Jo Pham <josephpham@ou.edu>
2020-10-06 12:00:39 -0500

    coded for the year

---
+++ Participations/CSVFile/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Principal;
+
+namespace CSVFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string filepath = @"C:\Users\jhpha\Downloads\Fall 2020 MIS 3013\sales_data_sample.csv";
+
+            string[] lines = File.ReadAllLines(filepath);
+
+            /*
+            *Sales = 4
+            *Status = 6
+            *Month = 8
+            *Year = 9
+            */
+
+            double sum = 0;
+            double sumFor2003 = 0;
+            double sumFor2004 = 0;
+            double sumFor2005 = 0;
+
+            for (int i = 1; i < lines.Length; i++)
+            { 
+                string line = lines[i];
+                string[] pieces = line.Split(',');
+                double sale = Convert.ToDouble(pieces[4]);
+                string year = pieces[9];
+                string month = pieces[8];
+                //sum = sum + Convert.ToDouble(pieces[4]);
+
+                if (pieces[6].Trim().ToLower() == "shipped")
+                {
+                    if (year == "2003")
+                    {
+                        sumFor2003 += sale;
+                    }
+                    else if (year == "2004")
+                    {
+                        sumFor2004 += sale;
+                    }
+                    else
+                    {
+                        sumFor2005 += sale;
+                    }
+                    sum += sale;
+                }
+            }
+            Console.WriteLine($"The total sales for shipped orders in 2003 is {sumFor2003.ToString("C2")}.");
+            Console.WriteLine($"The total sales for shipped orders in 2004 is {sumFor2004.ToString("C2")}.");
+            Console.WriteLine($"The total sales for shipped orders in 2005 is {sumFor2005.ToString("C2")}.");
+            Console.WriteLine($"Total sales of shipped orders is {sum.ToString("C2")}.");
+            Console.ReadKey();
+        }
+    }
+}
