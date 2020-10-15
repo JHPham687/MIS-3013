0918f24456e6f100688aac81b6077d4b13faca16
Jo Pham <josephpham@ou.edu>
2020-10-15 11:44:14 -0500

    Completed Program

--- Participations/CSVFile2/Program.cs
+++ Participations/CSVFile2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CSVFile2
 {
@@ -6,9 +7,29 @@ namespace CSVFile2
     {
         static void Main(string[] args)
         {
+            string filepath = @"C:SalesJan2009.csv";
+
+            string[] lines = File.ReadAllLines(filepath);
 
 
-            Console.ReadLine();
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] pieces = line.Split(',');
+                string name = pieces[4];
+                string transactionDate = pieces[0];
+                string country = pieces[7];
+                string paymentType = pieces[3];
+
+                if (pieces[3] == "Visa" && pieces[7] == "United States")
+                {
+                    Console.Write(transactionDate);
+                    Console.WriteLine(" " + name);
+                }
+            }
+
+ 
+            Console.ReadKey();
         }
     }
 }
