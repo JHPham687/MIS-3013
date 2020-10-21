58bb8ac307920ce861c24245b0ba94124312c452
Jo Pham <josephpham@ou.edu>
2020-10-20 21:50:53 -0500

    started methods program

---
+++ Participations/SimpleMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a type of animal.");
+
+            string animal = Console.ReadLine();
+        }
+        static void Speak(string[] args)
+        {
+
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
