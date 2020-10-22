4fc20342875ba8d77b6fbf80471d99314bcc5d13
Jo Pham <josephpham@ou.edu>
2020-10-22 12:26:34 -0500

    Completed program

--- Participations/SimpleMethod/Program.cs
+++ Participations/SimpleMethod/Program.cs
@@ -6,17 +6,32 @@ namespace SimpleMethod
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a type of animal.");
+            Console.WriteLine("Enter an animal. Valid answers are 'dog', 'cat' and 'goat'.");
 
             string animal = Console.ReadLine();
+            string s = Speak(animal.ToLower());
+            Console.WriteLine(s);
+            Console.ReadLine();
         }
-        static void Speak(string[] args)
+        static string Speak(string answer)
         {
 
-            
-
-
-            Console.ReadKey();
+            if (answer == "dog")
+            {
+                return ("bark");
+            }
+            else if (answer == "cat")
+            {
+                return ("meow");
+            }
+            else if (answer == "goat")
+            {
+                return ("bahh");
+            }
+            else
+            {
+                return("That is not a valid input");
+            }
         }
     }
 }
