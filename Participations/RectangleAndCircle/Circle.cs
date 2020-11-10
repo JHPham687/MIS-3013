d08d393e27065f7d581a22703eccd3f018eb2d04
Jo Pham <josephpham@ou.edu>
2020-11-10 11:58:48 -0600

    Completed Circles

---
+++ Participations/RectangleAndCircle/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RectangleAndCircle
+{
+    class Circle
+    {
+        //private double _Radius;
+
+        //public double GetRadius()
+        //{
+        //    return _Radius;
+        //}
+
+        public double Radius { get; set; }
+
+
+        public Circle()
+        {
+            Radius = 0;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        
+            
+
+    }
+}
---
+++ Participations/RectangleAndCircle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RectangleAndCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle myCircle = new Circle();
+            myCircle.Radius = 7.5;
+            Console.WriteLine($"The area of the circle with {myCircle.Radius} is {myCircle.CalculateArea()}\n" +
+            $" and a perimeter of {myCircle.CalculatePerimeter()}");
+
+            Circle circle2 = new Circle();
+            circle2.Radius = 8282828282;
+            double areaOfCircle2 = circle2.CalculateArea();
+            double perimeterOfCircle2 = circle2.CalculatePerimeter();
+
+            Console.ReadKey();
+
+        }
+    }
+}
