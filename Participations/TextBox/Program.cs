e9c3c278c90f162e2df232f2f15ce76112e16457
Jo Pham <josephpham@ou.edu>
2020-11-30 20:54:49 -0600

    Set up class

--- Participations/TextBox/Program.cs
+++ Participations/TextBox/Program.cs
@@ -6,7 +6,17 @@ namespace TextBox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TextBox textbox1 = new TextBox(1, ConsoleColor.DarkBlue, ConsoleColor.White, "This is text box 1");
+
+            TextBox textbox2 = new TextBox(1, ConsoleColor.Yellow, ConsoleColor.Green, "This is text box 2");
+
+            Console.WriteLine("Write a sentence for textbox 1");
+            string response1 = Console.ReadLine();
+
+
+
+            Console.ReadKey();
+
         }
     }
 }
---
+++ Participations/TextBox/TextBox.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextBox
+{
+    class TextBox
+    {
+        public int Padding { get; set; }
+        public ConsoleColor BackColor { get; set; }
+        public ConsoleColor FrontColor { get; set; }
+        public String Text { get; set; }
+
+        public TextBox(int padding, ConsoleColor backcolor, ConsoleColor frontcolor, string text)
+        {
+            Padding = padding;
+            BackColor = backcolor;
+            FrontColor = frontcolor;
+            Text = text;
+        }
+
+        public void DisplayText()
+        {
+            Console.WriteLine(TextBox);
+        }
+
+    }
+}
